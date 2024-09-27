67336e1e42b6a59ad1b39d596193e4ab8619addc
andresserrato2004 <andres.serrato-c@mail.escuelaing.edu.co>
2024-09-27 14:42:04 -0500

    feat: add buttons delete and acept

--- src/script.js
+++ src/script.js
@@ -1,77 +1,106 @@
-document.addEventListener("DOMContentLoaded", () => {
-	const mainView = document.getElementById("main-view");
-	const createView = document.getElementById("create-view");
-	const createBtn = document.getElementById("create-btn");
-	const createForm = document.getElementById("create-form");
-	const taskList = document.getElementById("task-list");
+const mainView = document.getElementById("main-view");
+const createView = document.getElementById("create-view");
+const createBtn = document.getElementById("create-btn");
+const createForm = document.getElementById("create-form");
+const taskList = document.getElementById("task-list");
 
-	const tasks = [];
+let tasks = [];
 
-	function renderTasks() {
-		taskList.innerHTML = "";
-		let task;
-		for (task of tasks) {
-			const row = document.createElement("tr");
-			row.innerHTML = `
-				<td>${task.id}</td>
-				<td>${task.nameTask}</td>
-				<td>${task.descriptionTask}</td>
-				<td>
-					<button class="complete-btn">✓</button>
-					<button class="delete-btn">✗</button>
-				</td>
-				`;
-			taskList.appendChild(row);
-		}
-	}
-
-	async function getTasks() {
-		try {
-			const task = await fetch("http://localhost:8080/api/tasks/all");
-			const ts = await task.json();
-			let tas;
-			for (tas of ts) {
-				tasks.push(tas);
-			}
-			renderTasks();
-			console.log("ts", tasks);
-		} catch (error) {
-			console.log("Error fetching tasks", error);
-		}
-	}
-
-	function completeTask(id) {
-		console.log("complete", id);
-	}
-
-	createBtn.addEventListener("click", () => {
-		mainView.style.display = "none";
-		createView.style.display = "block";
-	});
-
-	createForm.addEventListener("submit", async (e) => {
-		e.preventDefault();
-		const taskName = document.getElementById("task-name").value;
-		const Description = document.getElementById("description").value;
-		const newTask = {
-			id: tasks.length + 1,
-			nameTask: taskName,
-			descriptionTask: Description,
-		};
-
-		fetch("http://localhost:8080/api/tasks/create", {
-			method: "POST",
+async function deleteTask(id) {
+	const response = await fetch(
+		`${"http://localhost:8080/api/tasks/delete"}/${id}`,
+		{
+			method: "DELETE",
 			headers: {
 				"Content-Type": "application/json",
 			},
-			body: JSON.stringify(newTask),
-		});
+		},
+	);
+	tasks = tasks.filter((task) => String(task.id) !== String(id));
+	renderTasks();
+}
 
+async function completeTask(id) {
+	const response = await fetch(
+		`${"http://localhost:8080/api/tasks/done"}/${id}`,
+		{
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		},
+	);
+	renderTasks();
+}
+
+function renderTasks() {
+	console.log("Rendering tasks: ", tasks);
+
+	taskList.innerHTML = "";
+	let task;
+	for (task of tasks) {
+		const row = document.createElement("tr");
+		row.innerHTML = `
+				<td>${task.id}</td>
+				<td>${task.nameTask}</td>
+				<td class="limited-width">${task.descriptionTask}</td>
+				<td class="limited-width">
+					<form onClick="completeTask(${task.id})" class="horizontal-form" action="http://localhost:8080/api/tasks/done/${task.id}">
+						<button class="complete-btn" type="button">✓</button>
+					</form>
+					|
+					<form onClick="deleteTask(${task.id})" class="horizontal-form" action="http://localhost:8080/api/tasks/delete/${task.id}">
+						<button type="button" class="delete-btn">✗</button>
+					</form>
+				</td>
+				`;
+		taskList.appendChild(row);
+	}
+}
+
+async function getTasks() {
+	try {
+		const task = await fetch("http://localhost:8080/api/tasks/all");
+		const ts = await task.json();
+		let tas;
+		for (tas of ts) {
+			tasks.push(tas);
+		}
 		renderTasks();
-		createView.style.display = "none";
-		mainView.style.display = "block";
-		createForm.reset();
+	} catch (error) {
+		console.log("Error fetching tasks", error);
+	}
+}
+
+createBtn.addEventListener("click", () => {
+	mainView.style.display = "none";
+	createView.style.display = "block";
+});
+
+createForm.addEventListener("submit", async (e) => {
+	e.preventDefault();
+	const taskName = document.getElementById("task-name").value;
+	const Description = document.getElementById("description").value;
+	const newTask = {
+		id: tasks.length + 1,
+		nameTask: taskName,
+		descriptionTask: Description,
+	};
+
+	fetch("http://localhost:8080/api/tasks/create", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(newTask),
 	});
-	getTasks();
+
+	createView.style.display = "none";
+	mainView.style.display = "block";
+	createForm.reset();
+	tasks.push(newTask);
 	renderTasks();
 });
+
+getTasks();
+renderTasks();
