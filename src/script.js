532c840cf9c15e08598149c14a7fe3744a8b2811
andresserrato2004 <andres.serrato-c@mail.escuelaing.edu.co>
2024-09-27 23:55:40 -0500

    conflicto resueltoa

--- src/script.js
+++ src/script.js
@@ -8,6 +8,13 @@ const generatedIds = new Set();
 
 let tasks = [];
 
+/**
+ * Generates a unique 5-character alphanumeric ID.
+ * The ID is composed of uppercase letters, lowercase letters, and digits.
+ * Ensures that the generated ID is unique by checking against a set of previously generated IDs.
+ *
+ * @returns {string} A unique 5-character alphanumeric ID.
+ */
 function generateId() {
 	const characters =
 		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -22,6 +29,18 @@ function generateId() {
 	return id;
 }
 
+/**
+ * Deletes a task by its ID.
+ *
+ * This function sends a DELETE request to the server to remove the task with the specified ID.
+ * After the task is successfully deleted, it updates the local tasks list, re-renders the tasks,
+ * and removes the ID from the generatedIds set.
+ *
+ * @async
+ * @function deleteTask
+ * @param {string} id - The ID of the task to be deleted.
+ * @returns {Promise<void>} - A promise that resolves when the task is deleted and the UI is updated.
+ */
 async function deleteTask(id) {
 	const response = await fetch(
 		`${"http://localhost:8080/api/tasks/delete"}/${id}`,
@@ -37,6 +56,12 @@ async function deleteTask(id) {
 	generatedIds.delete(id);
 }
 
+/**
+ * Marks a task as complete by sending a PUT request to the server.
+ *
+ * @param {number} id - The ID of the task to be marked as complete.
+ * @returns {Promise<void>} A promise that resolves when the task is marked as complete.
+ */
 async function completeTask(id) {
 	const response = await fetch(
 		`${"http://localhost:8080/api/tasks/done"}/${id}`,
@@ -50,6 +75,16 @@ async function completeTask(id) {
 	getTasks();
 }
 
+/**
+ * Renders the list of tasks into the task list element.
+ * Clears the current content of the task list and populates it with rows representing each task.
+ * Each row contains the task ID, task name, task description, and action buttons for completing and deleting the task.
+ *
+ * @function renderTasks
+ * @example
+ * // Assuming `tasks` is an array of task objects and `taskList` is a DOM element:
+ * renderTasks();
+ */
 function renderTasks() {
 	console.log("Rendering tasks: ", tasks);
 
@@ -76,6 +111,19 @@ function renderTasks() {
 	}
 }
 
+/**
+ * Fetches tasks from the server and renders them.
+ *
+ * This function makes an asynchronous request to the server to retrieve all tasks.
+ * If the request is successful, it populates the `tasks` array with the fetched tasks
+ * and calls the `renderTasks` function to display them. If an error occurs during the
+ * fetch operation, it logs an error message to the console.
+ *
+ *
+ * @async
+ * @function getTasks
+ * @returns {Promise<void>} A promise that resolves when the tasks have been fetched and rendered.
+ */
 async function getTasks() {
 	tasks = [];
 	try {
