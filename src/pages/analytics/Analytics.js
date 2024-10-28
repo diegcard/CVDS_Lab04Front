8d46caaadfbba9e2b6a55037a6498e16b0140628
Zayra Gutierrrez <zayra.gutierrez-s@mail.escuelaing.edu.co>
2024-10-27 21:02:20 -0500

    fix

--- src/pages/analytics/Analytics.js
+++ src/pages/analytics/Analytics.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../../assets/styles/Analytics.css';
 import { API_BASE_URL } from '../../config/globals.js';
-import { TaskService } from "../../services/TaskService";
 
 const Analytics = () => {
     const [selectedChart, setSelectedChart] = useState(null);
